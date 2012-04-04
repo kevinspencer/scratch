e0177dd3f16fbc4c3796985d5bd44520f7609ff5
Kevin Spencer <kevin@kevinspencer.org>
2012-04-03 22:27:31 -0700

    stuffs

--- poe-wr.pl
+++ poe-wr.pl
@@ -4,7 +4,7 @@ use POE qw(Wheel::Run);
 use strict;
 use warnings;
 
-my $child_prog = '~/code/scratch/run-me.pl';
+my $child_prog = "~/code/scratch/run-me.pl";
 
 POE::Session->create(
     inline_states => {
@@ -18,13 +18,22 @@ exit();
 sub do_start {
     my $child = POE::Wheel::Run->new(
         Program => [$child_prog],
-        StdoutEvent => 'handle_child_stdout',
-        StderrEvent => 'handle_child_stderr',
-        CloseEvent  => 'handle_child_close'
+        StdoutEvent => "handle_child_stdout",
+        StderrEvent => "handle_child_stderr",
+        CloseEvent  => "handle_child_close"
     );
 
-    $_[KERNEL]->sig_child($child->PID(), 'handle_child_close');
+    $_[KERNEL]->sig_child($child->PID(), "handle_child_close");
 
     $_[HEAP]{children_by_wid}{$child->ID()} = $child;
     $_[HEAP]{children_by_pid}{$child->ID()} = $child;
+
+    print "started PID [" . $child->PID() . "] as WID [" . $child->ID() . "]", "\n";
+}
+
+sub handle_child_stderr {
+    my ($line, $wid) = @_[ARG0, ARG1];
+
+    my $child = $_[HEAP]{children_by_wid}{$wid};
+    print "child [" . $child->PID() . "] STDERR: $line\n";
 }
